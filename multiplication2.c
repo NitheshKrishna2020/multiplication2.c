cb0a1088802f81fff53d7c4f4c819546161efe21
NitheshKrishna2020 <s2000503@students.villacollege.edu.mv>
2021-03-14 09:24:08 +0530

    ain

---
+++ multiplication2.c
@@ -0,0 +1,9 @@
+nt main()
+{
+	int num1,num2,product;
+	printf("Enter two numbers:");
+	scanf("%d %d",&num1,&num2);
+	product=num1*num2;
+	printf("Product of two numbers: %d",product);
+	return  0;
+}
